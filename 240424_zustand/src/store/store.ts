be21ec887ea88bc529d516b37269aae4feaf8af7
heeyoung <9hiyoung@gmail.com>
2024-04-24 11:24:45 +0900

    Fix: devtools 수정 및 이름 지정

--- 240424_zustand/src/store/store.ts
+++ 240424_zustand/src/store/store.ts
@@ -1,6 +1,11 @@
 import { create, StateCreator } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
+const enum counterActions {
+  INCREASE = 'counter/increase',
+  DECREASE = 'counter/decrease',
+}
+
 export interface CountState {
   count: number
   increaseCount: () => void
@@ -11,14 +16,25 @@ const counterStore: StateCreator<CountState, [['zustand/devtools', never]]> = (
   set,
 ) => ({
   count: 0,
-  increaseCount: () => set((state) => ({ count: state.count + 1 })),
-  decreaseCount: () => set((state) => ({ count: state.count - 1 })),
+  increaseCount: () =>
+    set(
+      (state) => ({ count: state.count + 1 }),
+      false,
+      counterActions.INCREASE,
+    ),
+  decreaseCount: () =>
+    set(
+      (state) => ({ count: state.count - 1 }),
+      false,
+      counterActions.DECREASE,
+    ),
 })
 
-// 프로덕션 환경에서는 DevTools를 비활성화합니다.
-const useCounter =
-  process.env.NODE_ENV === 'production'
-    ? create<CountState>(counterStore)
-    : create(devtools(counterStore, { name: 'CounterStore' }))
+const useCounter = create(
+  devtools(counterStore, {
+    name: 'CounterStore',
+    enabled: !!(process.env.NODE_ENV !== 'production'),
+  }),
+)
 
 export default useCounter
